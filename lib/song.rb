878fd219d07fb5121041d2cb2042978e7553c4c8
hburdge1 <95104319+hburdge1@users.noreply.github.com>
2022-02-02 09:21:24 -0800

    commit tree 98a064c592cb77e8776b78ae881757fa1fe9df95

---
+++ lib/song.rb
@@ -0,0 +1,59 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count=0
+    @@genres=[]
+    @@artists=[]
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+
+        @@count += 1
+        @@genres.push(@genre)
+        @@artists.push(@artist)
+    end
+
+    def self.count
+        return @@count
+    end
+
+    def self.genres
+        unique_genres = []
+        @@genres.each do |genre|
+            if unique_genres.include?(genre) == false
+                unique_genres.push(genre)
+            end
+        end
+        return unique_genres
+    end
+
+    def self.artists
+        unique_artists = []
+        @@artists.each do |artist| 
+            if unique_artists.include?(artist) == false
+               unique_artists.push(artist)
+            end
+        end
+        return unique_artists
+    end
+
+    def self.artist_count
+        a_hash = Hash.new(0)
+            @@artists.each do |artist, value| 
+                    a_hash[artist] += 1
+            end
+        
+        a_hash    
+    end
+      
+    def self.genre_count
+        g_hash = Hash.new(0)
+            @@genres.each do |genre, value| 
+                    g_hash[genre] += 1
+            end
+        
+        g_hash    
+    end
+
+    
+end
